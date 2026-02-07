36c0b0f016ffc6676606c537edb1e48e7f1266fa
g4ek <rath.s22@outlook.com>
2026-02-07 12:55:59 -0800

    mostly functioning
    killzone, player movement, coin pickups, moving platforms, adaptive camera, etc.
    
    note: turn off sync to physics for moving stuff

---
+++ scripts/killzone.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer # referencing the Timer node below 
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print("You Died!")
+	timer.start() # begins the reload delay
+
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene() # reload current scenes in scene tree
