83957ea902ec29826b707f580ac5cc33c364cb5f
samrath0 <rath.s22@outlook.com>
2026-02-07 20:14:39 -0800

    added a little more to the world

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -7,5 +7,5 @@ func _on_body_entered(body: Node2D) -> void:
 	print("You Died!")
 	timer.start() # begins the reload delay
 
-func _on_timer_timeout() -> void:
+func _on_timer_timeout() -> void: 
 	get_tree().reload_current_scene() # reload current scenes in scene tree
