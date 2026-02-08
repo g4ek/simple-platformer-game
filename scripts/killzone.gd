7830cc56fbd22d8b07369bd54bc8a4a2e150a996
g4ek <rath.s22@outlook.com>
2026-02-07 21:52:50 -0800

    quality updates + conflict
    background, better player movement, enemies, text

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -5,7 +5,10 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You Died!")
+	Engine.time_scale = 0.5
+	body.get_node('CollisionShape2D').queue_free()
 	timer.start() # begins the reload delay
 
 func _on_timer_timeout() -> void: 
+	Engine.time_scale = 1
 	get_tree().reload_current_scene() # reload current scenes in scene tree
